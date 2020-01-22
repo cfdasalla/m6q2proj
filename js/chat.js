0cde8fe184fffdf4aae50a3356e4493c7d924332
lcpdecastro <40616595+lcpdecastro@users.noreply.github.com>
2020-01-22 22:42:14 +0800

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -329,7 +329,7 @@ ${dropsS}
 			MathJax.typeset();
 
 			for (let c of h.find(".drag-choice")) {
-				// $(c).on('touchmove', document.preventDefault, {passive: false});
+				$(c).on('touchmove', document.preventDefault, {passive: false});
 
 				$(c).on('dragstart', function (e) {
 					e.originalEvent.dataTransfer.setData("text/plain", $(e.originalEvent.target).attr("class").match(/drag-choice-./)[0].slice(-1));
